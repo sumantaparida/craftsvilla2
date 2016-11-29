001c88051deda8ced70a6576ded93b27a38eb343
Sumanta Parida <sumanta@Craftsvillas-MacBook-Air-4.local>
2016-11-29 18:16:34 +0530

    Home page

--- src/js/material-effect.js
+++ src/js/material-effect.js
@@ -3,33 +3,35 @@
  * ========================================================================
  * Copyright 2016.
  * ======================================================================== */
-+function ($) {
-  'use strict';
+ +function ($) {
+   'use strict';
+   // BUTTON WAVE EFFECT
+   // ============================================================
+   console.log("Material Button");
+   // Button Effect start
+   var ink, d, x, y;
+ 	$(".ripplelink").click(function(e){
+     e.preventDefault();
+     setTimeout(function(e) {
+       window.location.href = $(this)[0].href;
+     }, 600);
+     if($(this).find(".ink").length === 0){
+         $(this).prepend("<span class='ink'></span>");
+     }
 
-  // BUTTON WAVE EFFECT
-  // ============================================================
-  console.log("Material Button");
-  // Button Effect start
-  var ink, d, x, y;
-	$(".ripplelink").click(function(e){
-    e.preventDefault();
-    if($(this).find(".ink").length === 0){
-        $(this).prepend("<span class='ink'></span>");
-    }
+     ink = $(this).find(".ink");
+     ink.removeClass("animate");
 
-    ink = $(this).find(".ink");
-    ink.removeClass("animate");
+     if(!ink.height() && !ink.width()){
+         d = Math.max($(this).outerWidth(), $(this).outerHeight());
+         ink.css({height: d, width: d});
+     }
 
-    if(!ink.height() && !ink.width()){
-        d = Math.max($(this).outerWidth(), $(this).outerHeight());
-        ink.css({height: d, width: d});
-    }
+     x = e.pageX - $(this).offset().left - ink.width()/2;
+     y = e.pageY - $(this).offset().top - ink.height()/2;
 
-    x = e.pageX - $(this).offset().left - ink.width()/2;
-    y = e.pageY - $(this).offset().top - ink.height()/2;
+     ink.css({top: y+'px', left: x+'px'}).addClass("animate");
+   });
+ // Effect End
 
-    ink.css({top: y+'px', left: x+'px'}).addClass("animate");
-  });
-// Effect End
-
-}(jQuery);
+ }(jQuery);
